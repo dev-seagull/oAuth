f521cfa815db9dd619e14678c58bbe0073fc771c
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-07-24 13:31:57 +0430

    Directory of auth.js was changed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const passport = require('passport');
 require('./src/auth');
 
@@ -7,6 +8,10 @@ function isLoggedIn(req, res, next) {
 }
 
 const app = express();
+app.use(session({ secret: 'cats' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get('/', (req, res) => {
     res.send('<a href="/auth/google">Authenticate with Google</a>')
