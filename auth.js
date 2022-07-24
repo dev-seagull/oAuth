bccc39362d16a9db866bc176cd9793eea0fd91ee
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-07-24 12:49:02 +0430

    Client ID and Secret ID was added to auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
+const GOOGLE_CLIENT_ID = '598428733571-b5ns38qjhrr3pbs510vtb3cpb300a7tb.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://yourdomain:3000/auth/google/callback",
+    callbackURL: "http://localhost:5000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
